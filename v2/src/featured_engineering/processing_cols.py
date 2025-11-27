4fe4ac556981ccab4a321af8f3741a0c84cdce8c
utsav87589 <gkundukundu987@gmail.com>
2025-11-26 19:48:33 -0500

    1. updated the one_hot, label_encoding, value_count functions, 2. added unique_value, columns_to_drop functions, 3. Testing left only for label_encoding function

--- v2/src/featured_engineering/processing_cols.py
+++ v2/src/featured_engineering/processing_cols.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import numpy as np
 
-def value_counts(df, cols) : 
-    values = {}
-    for col in cols : 
-        values[col] = df[col].value_counts()
-    return values
+def unique_values(df, col) : 
+    return df[col].unique()
 
-def one_hot(df, cols) : 
-    for col in cols : 
-        dummies = pd.get_dummies(df[col], dtype = int, drop_first = True, prefix = col)
-        df = pd.concat([df, dummies], axis = 1)
+def value_counts(df, col) : 
+    return df[col].value_counts()
+
+def one_hot(df, col) : 
+    dummies = pd.get_dummies(df[col], dtype = int, drop_first = True, prefix = col)
+    df = pd.concat([df, dummies], axis = 1)
     return df
 
-# def label_encoding(df, cols, mapping) : 
-#     for col in cols : 
-#         df[col] = df[col].apply(mapping)
-#     return df
+def label_encoding(df, col, mapping) : 
+    df[col] = df[col].apply(mapping)
+    return df
 
+def columns_to_drop(df, cols) : 
+    df.drop(cols, axis = 1, inplace = True)
+    return df
