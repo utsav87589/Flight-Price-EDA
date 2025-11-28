9202ca5a72278407f98e48319828b61d99026b07
utsav87589 <gkundukundu987@gmail.com>
2025-11-28 18:35:35 -0500

    1. created and tested the split_numerical function for date_of_journey column, 2. Added comments for each function

--- v2/src/featured_engineering/processing_cols.py
+++ v2/src/featured_engineering/processing_cols.py
@@ -1,21 +1,54 @@
 import pandas as pd
 import numpy as np
 
+
+#--------------return the unique values in a column
 def unique_values(df, col) : 
     return df[col].unique()
 
+
+#--------------return the value counts of the column
 def value_counts(df, col) : 
     return df[col].value_counts()
 
+
+#-------------one hot encoding for the categorical columns
 def one_hot(df, col) : 
     dummies = pd.get_dummies(df[col], dtype = int, drop_first = True, prefix = col)
     df = pd.concat([df, dummies], axis = 1)
     return df
 
+
+#-------------label encoding for the categorical columns
 def label_encoding(df, col, mapping) : 
     df[col] = df[col].map(mapping)
     return df
 
+
+#---------------spliting the numerical column like Date_of_journey, Dep_time etc..
+def split_numerical(df, col) : 
+    sample_value = str(df[col].iloc[0])
+
+    if '/' in sample_value : 
+        sep = '/'
+        new_cols = ['day', 'month', 'year']
+
+    elif ':' in sample_value : 
+        sep = ':'
+        new_cols = ['hour', 'minute']
+
+    else : 
+        raise ValueError((f"{col} doesn't conatin '/' or ':'"))
+    
+    split_df = df[col].str.split(sep, expand = True)
+
+    for i in range(split_df.shape[1]) : 
+        df[f"{col}_{new_cols[i]}"] = split_df[i]
+
+    return df
+
+
+#---------------dropping the particular column
 def columns_to_drop(df, cols) : 
     df.drop(cols, axis = 1, inplace = True)
     return df
