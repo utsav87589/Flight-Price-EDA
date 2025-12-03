3d2da8df5fafc74fac9d65de8eebc748e5a2f173
utsav87589 <gkundukundu987@gmail.com>
2025-12-03 18:44:02 -0500

    python file to plot the graphs for the different features have been added

---
+++ v2/src/featured_engineering/plot_features.py
@@ -0,0 +1,36 @@
+#---------------importing the libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pylab
+import scipy.stats as stat
+
+
+#------------writing the function to check the plot for the individual column
+def plot_graphs(df, feature) : 
+
+    #---------first plot, which is a hist plot
+    plt.figure(figsize = (12, 4))
+    plt.subplot(1, 3, 1)
+    plt.title('Hist plot')
+    df[feature].hist()
+    plt.xlabel("")
+    plt.ylabel("")
+
+    #----------second one, which is a qq plot
+    plt.subplot(1, 3, 2)
+    plt.title('QQ plot')
+    stat.probplot(df[feature], dist = 'norm', plot = pylab)
+    plt.xlabel("")
+    plt.ylabel("")
+    
+    #----------third plot, box plot
+    plt.subplot(1, 3, 3)
+    plt.title('Boxplot')
+    sns.boxplot(df[feature])
+    plt.xlabel("")
+    plt.ylabel("")
+
+    plt.show()
