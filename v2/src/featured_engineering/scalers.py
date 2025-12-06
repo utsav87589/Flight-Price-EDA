b60918c3b87c6146f70b956342f244ad8bf4ef18
utsav87589 <gkundukundu987@gmail.com>
2025-12-06 18:22:38 -0500

    added a paramter in the function to avoid making the additional scaler object while working with df_test file, now it uses the pre saved file based on the paramters

--- v2/src/featured_engineering/scalers.py
+++ v2/src/featured_engineering/scalers.py
@@ -3,11 +3,14 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 
-scaler = MinMaxScaler(feature_range = (0, 1))
-
 #------------- function to apply the Min max scaler and then saving it
-def scale_feature(df, cols) : 
+def scale_feature(df, cols, scaler_path = None) : 
    #----------applying the scaler
+
+  if scaler_path is None : 
+
+    scaler = MinMaxScaler(feature_range = (0, 1))
+
     df[cols] = scaler.fit_transform(df[cols])
 
     #---------saving the scaler according to the column it was applied on
@@ -18,3 +21,7 @@ def scale_feature(df, cols) :
     else : 
       scaler_path = '../../../scalers/scaler_for_rest.pkl'
       joblib.dump(scaler, scaler_path)
+
+  else : 
+    scaler = joblib.load(scaler_path)
+    df[cols] = scaler.fit_transform(df[cols])
