3ac64572f7be697f1a3da946dc7ce45dfea9695b
utsav87589 <gkundukundu987@gmail.com>
2025-12-21 14:23:56 -0500

    pca file with functions to reduce the dimensons, explaining the correlation and the variance ratio added + tested

---
+++ v2/UnsupervisedML/src/unsupervised_ml/pca.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+#----------------function to find the correlation between the features
+
+def get_corr(df) : 
+    corr = df.corr()
+
+    return corr
+
+#----------------function to return the explained ratio
+def pca_and_variance(df) : 
+    pca = PCA()
+
+    pca.fit_transform(df)
+
+    arr = np.array(pca.explained_variance_ratio_)
+    np.set_printoptions(suppress = True)
+
+    return arr
+
+
+#--------------function to reduce the dimensionality of the datagrame to 2
+def reduce_dim(df) : 
+    pca = PCA(n_components = 2)
+
+    df_pca = pca.fit_transform(df)
+
+    return df_pca
