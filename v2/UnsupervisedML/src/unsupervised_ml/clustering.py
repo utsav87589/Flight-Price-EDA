1cbcea8c1a9799d657deaf95c51b6a489c948ade
utsav87589 <gkundukundu987@gmail.com>
2025-12-22 16:38:09 -0500

    added and tested the function for the db scan, dendogram work will resume tomorrow

--- v2/UnsupervisedML/src/unsupervised_ml/clustering.py
+++ v2/UnsupervisedML/src/unsupervised_ml/clustering.py
@@ -31,3 +31,48 @@ def find_k(df) :
 
     print(wcss)
     plt.plot(range(1, 12), wcss)
+
+#-----------function to plot the scatter plots for the value of the k
+def kmeans_clutsers(df, k) : 
+
+    kmeans = KMeans(n_clusters = k, init = 'k-means++')
+    y_labels = kmeans.fit_predict(df)
+
+    results(df, y_labels = y_labels)
+
+
+#========================= DBScan clustering =========================
+
+#-------------function to find the value of eps
+def find_eps(df) : 
+
+    neigh = NearestNeighbors(n_neighbors = 5)
+    nbrs = neigh.fit(df) 
+    distances, indices = neigh.kneighbors(df)
+
+    distances = np.sort(distances[:, 4])
+
+    plt.plot(distances)
+
+#------------function to plot the clusters for the value of eps
+def db_clusters(df, eps) : 
+    db_scan = DBSCAN(eps = eps)
+    y_labels_db = db_scan.fit_predict(df)
+
+    results(df, y_labels = y_labels_db)
+
+
+#========================= HM clustering =========================
+
+#-----------------function to make sample of 3000 records from the original data for the hm
+def sample_for_hm (df, n_samples = 3000, random_state = 42) : 
+    return df.sample(n = n_samples, random_state = random_state)
+
+#----------------function to plot dendogram for the value of k
+def plot_dendo(df_hm) : 
+    plt.figure(figsize=(20,7))
+    plt.title("Dendogram")
+    sc.dendrogram(sc.linkage(df_hm, method = 'ward'))
+    plt.title('Dendogram')
+    plt.xlabel("sample index")
+    plt.ylabel("euclidian distance")
