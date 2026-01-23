c8119adf2fcdecd8c12c824d1fbc4a7056c4ecd6
utsav87589 <gkundukundu987@gmail.com>
2026-01-22 21:36:51 -0500

    function to apply one hot encoding added and tested

--- v2/Supervised ML/src/feature_engineering/process_categoricals.py
+++ v2/Supervised ML/src/feature_engineering/process_categoricals.py
@@ -6,4 +6,24 @@ import numpy as np
 def get_nunique_value_counts(df, cols) : 
 
     for col in cols : 
-        print(f"{df[col].unique()} \n{df[col].value_counts()}")
+        print(f"{df[col].nunique()} \n{df[col].value_counts()}")
+
+
+### function to apply the one hot encoding to the features
+
+def apply_one_hot(df, cols) : 
+
+    for col in cols : 
+
+        dummies = pd.get_dummies(df[col], dtype = int, prefix = col, prefix_sep = '_', drop_first = True)
+
+        df = pd.concat([df, dummies], axis = 1)
+
+        df.drop(col, axis = 1, inplace = True)
+
+    return df
+
+
+
+### function to apply the label encoding to the given column
+
