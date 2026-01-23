c8cf68a906f521e15b69e20bb6f8a7fb2b35ce3a
utsav87589 <gkundukundu987@gmail.com>
2026-01-22 21:36:18 -0500

    function to drop duplicates added and tested as well

--- v2/Supervised ML/src/feature_engineering/handling_duplicates.py
+++ v2/Supervised ML/src/feature_engineering/handling_duplicates.py
@@ -7,3 +7,10 @@ def get_duplicates(df) :
 
     # print(f"{df.duplicated().sum()} \n{df[df.duplicated(keep = False)]}")
     return df[df.duplicated(keep = False)]
+
+### function to drop the duplicate value
+def drop_duplicates(df) : 
+
+    df = df.drop_duplicates(keep = False)
+
+    return df
