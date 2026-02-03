bbac8c35054dd8cc5a230e7c9825ed20de06b376
utsav87589 <gkundukundu987@gmail.com>
2026-02-02 19:47:39 -0500

    file with functions inside to process the numerical features including : spliting them, finding the different formats etc. added and tested

---
+++ v2/Supervised ML/src/feature_engineering/process_numericals.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+### function to check for the values, in a different format in the given numerical column
+def get_format(df, col, patterns) : 
+
+    print(df[col].astype(str).str.len().value_counts())
+
+    for name, pat in patterns.items():
+        count = df[col].astype(str).str.match(pat).sum()
+        print(name, ":", count)
+
+
+### function to split the features, based on the operator given
+def split_numericals(df, col, sep, new_cols) : 
+
+    #-------------splitting based on the given operator
+
+    split_df = df[col].str.split(sep, expand = True)
+
+    for i in range(split_df.shape[1]) : 
+        df[f"{col}_{new_cols[i]}"] = split_df[i]
+
+    #-----------then dropping the old column
+
+    df.drop(col, axis = 1, inplace = True)
+
+
+### function to check the unique values i.e. sometimes after split, we might wanna drop the further column, if it has no diversity
+def check_value_counts_numericals(df, cols) : 
+
+    for col in cols : 
+        print(f"{df[col].nunique()}")
+
+
+### function to drop the column from the newly created numerical columns
+def drop_cols_numerical(df, col) : 
+    
+    df.drop(col, axis = 1, inplace = True)
