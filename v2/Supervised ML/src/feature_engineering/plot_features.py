84d13acebdd2328f392f4a1d2f3a03a51a17e852
utsav87589 <gkundukundu987@gmail.com>
2026-02-06 20:16:36 -0500

    plot_graphs function now, can plot graphs for mutiple columns at the same time, earlier it was just 1 column in each call

--- v2/Supervised ML/src/feature_engineering/plot_features.py
+++ v2/Supervised ML/src/feature_engineering/plot_features.py
@@ -9,31 +9,34 @@ import scipy.stats as stat
 
 
 #------------writing the function to check the plot for the individual column
-def plot_graphs(df, feature) : 
+def plot_graphs(df, cols) : 
 
-    #---------first plot, which is a hist plot
-    plt.figure(figsize = (12, 4))
-    plt.subplot(1, 3, 1)
-    plt.title('Hist plot')
-    df[feature].hist()
-    plt.xlabel("")
-    plt.ylabel("")
+    for col in cols : 
 
-    #----------second one, which is a qq plot
-    plt.subplot(1, 3, 2)
-    plt.title('QQ plot')
-    stat.probplot(df[feature], dist = 'norm', plot = pylab)
-    plt.xlabel("")
-    plt.ylabel("")
-    
-    #----------third plot, box plot
-    plt.subplot(1, 3, 3)
-    plt.title('Boxplot')
-    sns.boxplot(df[feature])
-    plt.xlabel("")
-    plt.ylabel("")
+        print(f"feature : {col}")
+        #---------first plot, which is a hist plot
+        plt.figure(figsize = (12, 4))
+        plt.subplot(1, 3, 1)
+        plt.title('Hist plot')
+        df[col].hist()
+        plt.xlabel("")
+        plt.ylabel("")
 
-    plt.show()
+        #----------second one, which is a qq plot
+        plt.subplot(1, 3, 2)
+        plt.title('QQ plot')
+        stat.probplot(df[col], dist = 'norm', plot = pylab)
+        plt.xlabel("")
+        plt.ylabel("")
+        
+        #----------third plot, box plot
+        plt.subplot(1, 3, 3)
+        plt.title('Boxplot')
+        sns.boxplot(df[col])
+        plt.xlabel("")
+        plt.ylabel("")
+
+        plt.show()
 
 #-----------writing the function to plot the box plots for the features after scaling to verify the things
 def plot_graphs_post_scaling(df, df_copy, feature) : 
