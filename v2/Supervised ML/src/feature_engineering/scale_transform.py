eb15883e8d663fe33f012882d4accb2d230343b2
utsav87589 <gkundukundu987@gmail.com>
2026-02-07 19:36:51 -0500

    functions to scale on valid, train data, function to drop the feature post scalingadded, function that apply boxcox transformation modified slightly

--- v2/Supervised ML/src/feature_engineering/scale_transform.py
+++ v2/Supervised ML/src/feature_engineering/scale_transform.py
@@ -9,4 +9,26 @@ import joblib
 def apply_transform_boxcox(df, cols) : 
 
     for col in cols : 
-        df[col], parameter = boxcox(df[col] + 1)
+        df[f"{col}_boxcox"], parameter = boxcox(df[col] + 1)
+
+
+### function to drop the old/new columns, if the transformation worked or not
+def drop_pre_post_transformation(df, cols) : 
+    
+    df.drop(cols, axis = 1, inplace = True)
+
+
+### function to scale the data and save the scaler on the train data.
+def scale_train(df, cols_to_scale, scaler_path) : 
+
+    scaler = MinMaxScaler(feature_range = (0, 1))
+
+    df[cols_to_scale] = scaler.fit_transform(df[cols_to_scale])
+    joblib.dump(scaler, scaler_path)
+
+
+### function to scale the valid data, by loading the scaler that was already fit on the train data
+def scale_valid(df, cols_to_scale, scaler_path) : 
+
+    scaler = joblib.load(scaler_path)
+    df[cols_to_scale] = scaler.fit_transform(df[cols_to_scale])
