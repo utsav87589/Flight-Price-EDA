63a9f44f6bac830cb0291be198955145f99c654d
utsav87589 <gkundukundu987@gmail.com>
2026-02-09 20:54:13 -0500

    split_data file in the src folder added

--- v2/Supervised ML/src/predictions/split_data.py
+++ v2/Supervised ML/src/predictions/split_data.py
@@ -1,21 +1,20 @@
-#---------------splitting the data and returning the train/test files
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-#------------function to split the data
-#------------if it has price column, then it is regular data else, it's test data as x_eval
-def data_split(df, price = None) : 
-    #------------if price column is there
-    if price is not None : 
-        X = df.drop('Price', axis = 1)
-        y = df['Price']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42, test_size = 0.25)
-        return X_train, X_test, y_train, y_test
-        
 
-    #-----------if price column in not there
-    else : 
-        X_eval = df
-        return X_eval
+### function to split the data
+def split_data(df, target_feature) : 
+        
+        X = df.drop(target_feature, axis = 1)
+        y = df[target_feature]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42, test_size = 0.25)
+
+        return X_train, X_test, y_train, y_test
+
+
+### function to verify the split (shapes basically)
+def verify_split(X_train, X_test, y_train, y_test) : 
+        
+        print(f"X_train : {X_train.shape} :: y_train : {y_train.shape} \nX_test : {X_test.shape} :: y_test : {y_test.shape}")
